13b3e40ee4a377019b18a044310cc623443b3fed
Hussain Roker <hussainroker@gmail.com>
2023-02-06 15:23:10 +0100

    added suggestion from Pavol; changed Buzz to Buss

--- task.js
+++ task.js
@@ -6,29 +6,29 @@
 
 const doFizz = () => {
     // alarm code here, or any other custom code
-    return "Fizz"
-}
+    return "Fizz";
+};
 
 const doNothing = () => {
     // alarm code here, or any other custom code
-    return ""
-}
+    return "";
+};
 
 const doBuzz = () => {
     // alarm code here, or any other custom code
-    return "Buss"
-}
+    return "Buss";
+};
 
 const print = (i) => {
-    console.log(i)
-}
+    console.log(i);
+};
 
 
-const isMultipleOf3 = i => i % 3 == 0 ? doFizz() : doNothing()
-const isMultipleOf5 = i => i % 5 == 0 ? doBuzz() : doNothing()
+const isMultipleOf3 = i => i % 3 == 0 ? doFizz() : doNothing();
+const isMultipleOf5 = i => i % 5 == 0 ? doBuzz() : doNothing();
 
 for (let i = 1; i <= 100; i++) {
-    let res = isMultipleOf3(i)
-    res += isMultipleOf5(i)
-    res ? print(res) : print(i)
+    let res = isMultipleOf3(i);
+    res += isMultipleOf5(i);
+    res ? print(res) : print(i);
 }
