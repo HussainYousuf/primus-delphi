6cb7901f53293a2db56f357e96f050e304de2097
Hussain Roker <hussainroker@gmail.com>
2023-02-06 15:20:25 +0100

    added suggestion from Pavol; changed Buzz to Buss

--- task.js
+++ task.js
@@ -3,11 +3,32 @@
 // "Buzz".
 // For numbers which are multiples of both three and five print "FizzBuzz".
 
-const isMultipleOf3 = i => i % 3 == 0
-const isMultipleOf5 = i => i % 5 == 0
+
+const doFizz = () => {
+    // alarm code here, or any other custom code
+    return "Fizz"
+}
+
+const doNothing = () => {
+    // alarm code here, or any other custom code
+    return ""
+}
+
+const doBuzz = () => {
+    // alarm code here, or any other custom code
+    return "Buss"
+}
+
+const print = (i) => {
+    console.log(i)
+}
+
+
+const isMultipleOf3 = i => i % 3 == 0 ? doFizz() : doNothing()
+const isMultipleOf5 = i => i % 5 == 0 ? doBuzz() : doNothing()
+
 for (let i = 1; i <= 100; i++) {
-    if (isMultipleOf3(i) && isMultipleOf5(i)) console.log("FizzBuzz")
-    else if (isMultipleOf3(i)) console.log("Fizz")
-    else if (isMultipleOf5(i)) console.log("Buzz")
-    else console.log(i)
+    let res = isMultipleOf3(i)
+    res += isMultipleOf5(i)
+    res ? print(res) : print(i)
 }
